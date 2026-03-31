118948e7aa770c47d6a81b827ca3ef34227ed10f
Aslbek <aslbekno07@gmail.com>
2026-03-31 10:22:13 +0500

    Add configuration for bot token and database connection

---
+++ config.py
@@ -0,0 +1,12 @@
+import pytz
+
+# SIZNING YANGI BOT TOKENINGIZNI SHU YERGA QO'YING:
+BOT_TOKEN = "YANGI_TOKEN_SHU_YERDA_BO'LADI"
+
+ADMIN_ID = 123456789  # O'zingizning Telegram ID raqamingiz
+
+# MongoDB ulanish kodi (dollar belgisiz parol bilan)
+MONGO_URL = "mongodb+srv://usa2001aqsh_db_user:Aslbek2007@cluster0.sf2njxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+TIMEZONE = pytz.timezone("Asia/Tashkent")
+  
