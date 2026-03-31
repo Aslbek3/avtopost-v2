e72ea8c6eb190230b0a38305593b61514585c132
Aslbek <aslbekno07@gmail.com>
2026-03-31 10:56:25 +0500

    Change MongoDB password in config.py
    Updated MongoDB connection string password.

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BOT_TOKEN = "8652336597:AAF6XzAsDfL1OWQ5IdhD9DVCYTlLsmHUI_8"
 ADMIN_ID = 7586510077  # O'zingizning Telegram ID raqamingiz
 
 # MongoDB ulanish kodi (dollar belgisiz parol bilan)
-MONGO_URL = "mongodb+srv://usa2001aqsh_db_user:Aslbek2007@cluster0.sf2njxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URL = "mongodb+srv://usa2001aqsh_db_user:Asl2007@cluster0.sf2njxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 TIMEZONE = pytz.timezone("Asia/Tashkent")
   
